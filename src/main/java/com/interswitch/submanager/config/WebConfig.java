ace74a5d229e0fda99b7e4d6620ba59a3e18cd54
Ernest Ehigiator <ernestehigiator@yahoo.com>
2022-11-24 16:25:40 +0100

    cors error

--- src/main/java/com/interswitch/submanager/config/WebConfig.java
+++ src/main/java/com/interswitch/submanager/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
     }
 }
